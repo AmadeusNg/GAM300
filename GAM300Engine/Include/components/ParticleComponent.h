c3893f6ce7969b838ecdffe4085ad37dc299ce7d
AmadeusNg <amadeusng96@gmail.com>
2024-03-07 16:02:55 +0800

    changed Emitters
    introduced min and max values to support randomness of particle spawning and movement

--- GAM300Engine/Include/components/ParticleComponent.h
+++ GAM300Engine/Include/components/ParticleComponent.h
@@ -22,13 +22,13 @@ namespace TDS {
 	};
 
 	struct ParticleEmitter {
-		float Life{10.f};
-		Vec3 Spawnoffset{0.f,0.f,0.f};
-		Vec3 Velocity{0.f,10.f,0.f};
-		Vec3 Acceleration{0.f,10.f,0.f};
+		float minlife{ 1.f }, maxlife{ 10.f };
+		Vec3 minSpawnoffset{ 0.f,0.f,0.f }, maxSpawnoffset{ 0.f,0.f,0.f };
+		Vec3 minVelocity{ 0.f,10.f,0.f }, maxVelocity{ 0.f,15.f,0.f };
+		Vec3 minAcceleration{ 0.f,10.f,0.f }, maxAcceleration{ 0.f,15.f,0.f };
+		Vec3 minSize{ 1.f,1.f,1.f }, maxSize{ 10.f,10.f,10.f };
 		Vec3 Position{ 0.f,0.f,0.f };
-		Vec3 Size{1.f,1.f,1.f};
-		Vec4 Color{0.f,1.f,1.f,1.f};
+		Vec4 Color{ 0.f,1.f,1.f,1.f };
 	};
 
 	struct Particle_Emitter_PushData {
@@ -41,31 +41,41 @@ namespace TDS {
 		DLL_API Particle_Component();
 		DLL_API ~Particle_Component() = default;
 
-	
+
 		//getter
-		float& GetLife()				{ return Emitter.Life; };
-		Vec3& GetSpawnoffset()			{ return Emitter.Spawnoffset; };
-		Vec3& GetVelocity()				{ return Emitter.Velocity; };
-		Vec3& GetAcceleration()			{ return Emitter.Acceleration; };
-		Vec3& GetPosition()				{ return Emitter.Position; };
-		Vec3& GetSize()					{ return Emitter.Size; };
-		Vec4& GetColor()				{ return Emitter.Color; };
-		ParticleEmitter& GetEmitter()	{ return Emitter; }
-		ParticleMesh& GetMeshType()		{ return type; }
-		float& GetSpawnInterval()		{ return spawninterval; }
-		float& GetSpawnTimer()			{ return spawntimer; }
-		uint32_t& GetMaxParticles()		{ return maxparticles; }
+		float& GetMinLife()						{ return Emitter.minlife; };
+		float& GetMaxLife()						{ return Emitter.maxlife; };
+		Vec3& GetMinSpawnoffset()				{ return Emitter.minSpawnoffset; };
+		Vec3& GetMaxSpawnoffset()				{ return Emitter.maxSpawnoffset; };
+		Vec3& GetMinVelocity()					{ return Emitter.minVelocity; };
+		Vec3& GetMaxVelocity()					{ return Emitter.maxVelocity; };
+		Vec3& GetMinAcceleration()				{ return Emitter.minAcceleration; };
+		Vec3& GetMaxAcceleration()				{ return Emitter.maxAcceleration; };
+		Vec3& GetMinSize()						{ return Emitter.minSize; };
+		Vec3& GetMaxSize()						{ return Emitter.maxSize; };
+		Vec3& GetPosition()						{ return Emitter.Position; };
+		Vec4& GetColor()						{ return Emitter.Color; };
+		ParticleEmitter& GetEmitter()			{ return Emitter; }
+		ParticleMesh& GetMeshType()				{ return type; }
+		float& GetSpawnInterval()				{ return spawninterval; }
+		float& GetSpawnTimer()					{ return spawntimer; }
+		uint32_t& GetMaxParticles()				{ return maxparticles; }
 
 		//setters
-		void SetLife(float& input)								{ Emitter.Life = input; }
-		void SetSpawnOffset(Vec3& input)						{ Emitter.Spawnoffset = input; }
-		void SetVelocity(Vec3& input)							{ Emitter.Velocity = input; }
-		void SetAcceleration(Vec3& input)						{ Emitter.Acceleration = input; }
+		void SetMinLife(float& input)			{ Emitter.minlife = input; }
+		void SetMaxLife(float& input)			{ Emitter.maxlife = input; }
+		void SetMinSpawnOffset(Vec3& input)		{ Emitter.minSpawnoffset = input; }
+		void SetMaxSpawnOffset(Vec3& input)		{ Emitter.maxSpawnoffset = input; }
+		void SetMinVelocity(Vec3& input)		{ Emitter.minVelocity = input; }
+		void SetMaxVelocity(Vec3& input)		{ Emitter.maxVelocity = input; }
+		void SetMinAcceleration(Vec3& input)	{ Emitter.minAcceleration = input; }
+		void SetMaxAcceleration(Vec3& input)	{ Emitter.maxAcceleration = input; }
+		void SetMinSize(Vec3& input)			{ Emitter.minSize = input; }
+		void SetMaxSize(Vec3& input)			{ Emitter.maxSize = input; }
+		void SetColor(Vec4& input)				{ Emitter.Color = input; }
+		void SetSpawnTimer(float& input)		{ spawntimer = input; }
 		//void SetPosition(Vec3 input)							{ Emitter.Position = input; }
-		void SetSize(Vec3& input)								{ Emitter.Size = input; }
-		void SetColor(Vec4& input)								{ Emitter.Color = input; }
-		void SetSpawnTimer(float& input)						{ spawntimer = input; }
-	
+
 		RTTR_ENABLE(IComponent);
 		RTTR_REGISTRATION_FRIEND;
 
